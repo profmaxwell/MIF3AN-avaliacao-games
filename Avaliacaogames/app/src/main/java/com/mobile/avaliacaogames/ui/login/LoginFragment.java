89a11aff47d09f960529c77da38e80b8c45695fc
SrAbnerP <abner.sol.pereira@gmail.com>
2023-03-22 17:23:33 -0300

    add tela de login

---
+++ Avaliacaogames/app/src/main/java/com/mobile/avaliacaogames/ui/login/LoginFragment.java
@@ -0,0 +1,25 @@
+package com.mobile.avaliacaogames.ui.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mobile.avaliacaogames.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+}
