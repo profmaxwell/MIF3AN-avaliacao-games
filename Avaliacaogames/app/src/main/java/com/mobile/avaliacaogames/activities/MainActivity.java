762d3a2fc6d8dd068415243d0746aea79c79b410
SrAbnerP <abner.sol.pereira@gmail.com>
2023-03-28 17:35:26 -0300

    add tela login e registrar

--- Avaliacaogames/app/src/main/java/com/mobile/avaliacaogames/activities/MainActivity.java
+++ Avaliacaogames/app/src/main/java/com/mobile/avaliacaogames/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobile.avaliacaogames;
+package com.mobile.avaliacaogames.activities;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mobile.avaliacaogames.R;
 import com.mobile.avaliacaogames.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
---
+++ Avaliacaogames/app/src/main/java/com/mobile/avaliacaogames/activities/RegistrarActivity.java
@@ -0,0 +1,16 @@
+package com.mobile.avaliacaogames.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mobile.avaliacaogames.R;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
