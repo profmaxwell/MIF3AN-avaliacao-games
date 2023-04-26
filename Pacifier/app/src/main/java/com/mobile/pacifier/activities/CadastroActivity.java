7911c4451a68b9672e34a706ca9a56678d4afcc5
SrAbnerP <abner.sol.pereira@gmail.com>
2023-04-26 14:03:40 -0300

    add validação de cpf ao cadastrar

--- Pacifier/app/src/main/java/com/mobile/pacifier/activities/CadastroActivity.java
+++ Pacifier/app/src/main/java/com/mobile/pacifier/activities/CadastroActivity.java
@@ -116,6 +116,11 @@ public class CadastroActivity extends AppCompatActivity {
             return;
         }
 
+        if (!validarCPF(String.valueOf(textoCpf))) {
+            Toast.makeText(CadastroActivity.this, "Digite um CPF válido!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         if (authService.checkIfExists("cpf", String.valueOf(textoCpf))) {
             editEmail.setError("Esse CPF já está cadastrado!");
             return;
@@ -143,6 +148,45 @@ public class CadastroActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public boolean validarCPF(String cpf) {
+        // Remove caracteres especiais do CPF
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        // Verifica se o CPF tem 11 dígitos
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        // Calcula o primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int resto = 11 - (soma % 11);
+        int digito1 = (resto >= 10) ? 0 : resto;
+
+        // Calcula o segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        resto = 11 - (soma % 11);
+        int digito2 = (resto >= 10) ? 0 : resto;
+
+        // Verifica se os dígitos verificadores estão corretos
+        if ((cpf.charAt(9) - '0' != digito1) || (cpf.charAt(10) - '0' != digito2)) {
+            return false;
+        }
+
+        // CPF é válido
+        return true;
     }
 
     private boolean validarNumero(String numero) {
